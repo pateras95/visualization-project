874f31a01ad6bcaa8638cd8563ad88ddfe3a4577
Konstantinos <kostasmc2@gmail.com>
2022-07-15 15:16:03 +0300

    some fixes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,6 +7,12 @@
             Online education has been widely applied for the last 2 years, due to the pandemic conditions. Initially it was perceived as a temporary solution to an urgent condition, but as years went by, students  became familiar with this new kind of education and started to adapt to it. Some may argue that online education is here to stay, initiating a new era in education from anywhere, anytime. However, is online education enough? No matter the overall students performance, online education is considered to be insufficient with regards to students' social interaction with each other, culture, ethics, etc. 
             This work aims to highlight and combine the facts into visualisations that will help us build a solid view on whether students are well adapted to online education and under which circumstances.
         </div>
+        <div class="simple-text">
+            Even though the issue has been widely discussed the past two years, we have not been able to find good variety of datasets
+            in order to form our visualizations, probably because the issue is relatively new.
+            We have utilized datasets from Unicef, Kaggle and Mendley as presented in the corresponding section,
+            and we tried to form visualizations that bring value to the stakeholders.
+        </div>
         <div class="text-center">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/neOL-QDAUIA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
